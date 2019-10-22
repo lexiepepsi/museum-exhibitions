7f919dbb291a725ea859ab15f847780a00dc92ce
alexabark <alexa@barkbox.com>
2019-10-22 15:22:59 -0400

    put it back

--- app/services/met_scraper.rb
+++ app/services/met_scraper.rb
@@ -12,38 +12,32 @@ class MetScraper
 
     titles = []
     general_content.css('p').each do |title|
-      titles << title.text
+      infos = title.text.split("\n")
+      infos.each do |info|
+        if !info.include?(",") && !info.include?("Location")
+          titles << info
+        end
+      end
     end
-    titles
 
-    # titles = []
-    # general_content.css('p').each do |title|
-    #   infos = title.text.split("\n")
-    #   infos.each do |info|
-    #     if !info.include?(",") && !info.include?("Location")
-    #       titles << info
-    #     end
-    #   end
-    # end
+    dates = []
+    general_content.css('p').each do |title|
+      infos = title.text.split("\n")
+      infos.each do |info|
+        if info.include?(",")
+          dates << info
+        end
+      end
+    end
 
-    # dates = []
-    # general_content.css('p').each do |title|
-    #   infos = title.text.split("\n")
-    #   infos.each do |info|
-    #     if info.include?(",")
-    #       dates << info
-    #     end
-    #   end
-    # end
+    combo = []
+    d = 0
+    titles.each do |title|
+      combo << title
+      combo << dates[d]
+      d += 1
+    end
 
-    # combo = []
-    # d = 0
-    # titles.each do |title|
-    #   combo << title
-    #   combo << dates[d]
-    #   d += 1
-    # end
-
-    # combo
+    combo
   end
 end
